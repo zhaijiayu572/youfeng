94f00f2ca90dfe22e1cd5696db9dfaccfb030ab2
fingertippower <939408507@qq.com>
2017-05-26 16:37:31 +0800

    1

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -1,9 +1,12 @@
 <?php
  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  class User extends CI_Controller{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('user_model');
-    }
- }
+    public function User(){  
+	    parent::__construct();
+	    $this->load->model('user_model'); 
+  } 
+  	public function aa(){
+  		$a = $this->user_model->get_data();
+  		var_dump($a);
+  }
+}
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,5 +2,12 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class User_model extends CI_Model
 {
-
+	public function User_model(){  
+        parent::__construct();  
+    }
+    public function get_data()  
+    {           
+        $query = $this->db->get('t_user');  
+        return $query;  
+    }   
 }
