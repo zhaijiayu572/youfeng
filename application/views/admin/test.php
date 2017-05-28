2498b2be25c94c5756c016437f2db14f494c3130
likeekil <1244403390@qq.com>
2017-05-28 22:24:56 +0800

    更新

--- application/views/admin/test.php
+++ application/views/admin/test.php
@@ -5,11 +5,6 @@
     <title>Title</title>
     <base href="<?php echo site_url()?>">
     <link rel="stylesheet" href="assets/css/reset.css">
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> e81a7cea56a46f27c5a68cba1b107277d71574a6
 </head>
 <body>
 <div id="app"></div>
