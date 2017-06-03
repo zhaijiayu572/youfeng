562f268ca4845e0f943096704c6f7c43f058fffc
fingertippower <939408507@qq.com>
2017-06-03 17:50:40 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	assets/src/admin/product.js

---
+++ assets/src/admin/product.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import product from '../../components/admin_product.vue'
+new Vue({
+    el: '#app',
+    render: h => h(product)
+});
--- assets/src/admin/user.js
+++ assets/src/admin/user.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import head from '../../components/user.vue'
+import head from '../../components/admin_user.vue'
 new Vue({
         el:'#app',
         render: h =>h(head)
