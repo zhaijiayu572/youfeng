3206256b6aade6b9d3eb8839b0a0664f5cfd04e0
fingertippower <939408507@qq.com>
2017-06-04 18:54:11 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	assets/src/admin/product.js

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -4,19 +4,34 @@
     public function User(){  
 	    parent::__construct();
 	    $this->load->model('user_model'); 
-  } 
-  	public function email(){
-  		$this->load->library('email');
-  		$this->email->sendMail('939408507@qq.com',"8520");
   }
     public function getMsg(){
-  	   $email   = $this->user_model->get_email(7)->user_email;
-       $company = $this->user_model->get_company(7)->user_company;
-       $phone   = $this->user_model->get_phone(7)->user_phone;
-       $name    = $this->user_model->get_name(7)->user_name;
-       $new_pass= $this->user_model->change_pass();
-       $msg = ['email'=>$email,'company'=>$company,'phone'=>$phone,'name'=>$name];
-       print_r($msg);
+  	   $phone = $_GET['phone'];
+  	   $msg = $this->user_model->get_msg($phone);
+       echo json_encode($msg);
+    }
+    public function sendEmail(){
+        $pass = $this->user_model->change_pass();
+        $email = $_GET['email'];
+        $this->load->library('email'); // 引入email类
+        // 配置选项
+        $config['protocol'] = 'smtp'; // 使用smtp协议
+        $config['smtp_host'] = 'smtp.126.com'; // smtp主机
+        $config['smtp_user'] = 'chuangdian_keji@126.com'; // smtp用户
+        $config['smtp_pass'] = 'wangtao123'; // 密码
+        $config['charset'] = 'utf-8';
+        $config['mailtype'] = 'html';
+        $config['crlf'] = "\r\n"; // 设置换行符
+        $config['newline'] = "\r\n"; // 设置换行符
+        $this->email->initialize($config); // 写入配置项
+
+        $this->email->from('chuangdian_keji@126.com'); // 发送者
+        $this->email->to($email); // 接收者
+
+        $this->email->subject('找芯网用户更改密码通知'); // 主题
+        $this->email->message('新的密码是'.$pass.'请尽快登录网站去修改密码'); // 内容
+
+        echo $this->email->send();
     }
     public function test(){
   	    $this->load->view('admin/test');
